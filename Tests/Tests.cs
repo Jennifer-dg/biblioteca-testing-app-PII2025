d79f3e4cc5317b9a936f749725fbde407c76f598
Jennifer-dg <dgaby0214@gmail.com>
2025-09-20 10:18:12 -0600

    cambios

--- Tests/Tests.cs
+++ Tests/Tests.cs
@@ -22,40 +22,60 @@ namespace Tests
         public void PrestarLibro_LibroDisponible_PrestaLibroCorrectamente()
         {
             // Act
+            _biblioteca.PrestarLibro(_libro1.Titulo);
 
             // Assert
+            Assert.IsFalse(_libro1.Equals(true));
         }
 
         [Test]
         public void PrestarLibro_LibroNoDisponible_LanzaExcepcion()
         {
             // Act
+            _biblioteca.PrestarLibro(_libro1.Titulo);
+
 
             // Assert
+            Assert.Throws<InvalidOperationException>(() => _biblioteca.PrestarLibro(_libro1.Titulo));
         }
 
         [Test]
         public void DevolverLibro_LibroPrestado_DevolveLibroCorrectamente()
         {
             // Act
+            _libro1.Prestar();
+            _libro2.Prestar();
+            _biblioteca.DevolverLibro(_libro1.Titulo);
+            _biblioteca.DevolverLibro(_libro2.Titulo);
 
             // Assert
+            Assert.IsTrue(_libro1.Equals(false));
+            Assert.IsFalse(_libro2.Equals(true));
+
         }
 
         [Test]
         public void DevolverLibro_LibroNoPrestado_LanzaExcepcion()
         {
             // Act
+            _libro1.Prestar();
+            _biblioteca.DevolverLibro(_libro1.Titulo);
+
+
 
             // Assert
+            Assert.Throws<InvalidOperationException>(() => _biblioteca.DevolverLibro(_libro1.Titulo));
+
         }
 
         [Test]
         public void ObtenerLibros_RetornaListaDeLibros()
         {
             // Act
+            var libros = _biblioteca.ObtenerLibros();
 
             // Assert
+            Assert.That(libros.Count, Is.EqualTo(2));
         }
     }
 }
